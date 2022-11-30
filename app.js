d386387bac2d238219ce5665b4e54e2b58144917
Shoaib Zeshan <szeeshan707@gmail.com>
2022-12-01 01:30:51 +0530

    navbar completed

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const dropDown = document.querySelectorAll('.drop-down-menu')
 const dropDownList = document.querySelectorAll('.navbar__drop-down-list')
-const levelTwo = document.querySelector('.dropdown-level-2')
+const levelTwo = document.querySelector('.navbar__dropdown-level-2')
 const subList = document.querySelector('.navbar__sub-list')
 const heroSection = document.querySelector('.hero-section')
+const navBtns = document.querySelectorAll('[dropdown-btn]')
+const hamIcon = document.querySelector('.navbar__ham')
+const navContent = document.querySelector('.navbar__content')
 
-console.log(dropDown)
+console.log(hamIcon)
 
 document.addEventListener('click', e => {
     const isDropdownBtn = e.target.matches('[dropdown-btn]')
@@ -14,18 +17,28 @@ document.addEventListener('click', e => {
     if (isDropdownBtn) {
         currentDropdown = e.target.closest("[data_dropdown]")
         currentDropdown.classList.toggle("active")
-        dropDown.forEach(menu => menu.classList.add('.active-color'))
 
     }
 
     document.querySelectorAll("[data_dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove("active")
-
     })
 
 })
 levelTwo.addEventListener('click', () => {
     subList.classList.toggle('show')
+    levelTwo.classList.toggle('clicked-color')
+
+})
+navBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active-color')
+    })
+})
+hamIcon.addEventListener('click', () => {
+    hamIcon.classList.toggle('rotate-icon')
+    navContent.classList.toggle('show-sidebar')
+
 
 })
