1c1fb35b1dc33af711183a82e08b80eae234b00a
Shoaib Zeshan <szeeshan707@gmail.com>
2022-11-30 23:17:27 +0530

    building sidebar

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const dropDown = document.querySelectorAll('.drop-down-menu')
 const dropDownList = document.querySelectorAll('.navbar__drop-down-list')
+const levelTwo = document.querySelector('.dropdown-level-2')
+const subList = document.querySelector('.navbar__sub-list')
+const heroSection = document.querySelector('.hero-section')
 
-console.log(dropDownList)
-dropDown.forEach((menu) => {
-    menu.addEventListener('click', () => {
-        dropDownList.forEach((list) => {
-            list.classList.remove('active')
-        })
+console.log(dropDown)
 
+document.addEventListener('click', e => {
+    const isDropdownBtn = e.target.matches('[dropdown-btn]')
+    if (!isDropdownBtn && e.target.closest("[data_dropdown]") != null) return
 
+    let currentDropdown
+    if (isDropdownBtn) {
+        currentDropdown = e.target.closest("[data_dropdown]")
+        currentDropdown.classList.toggle("active")
+        dropDown.forEach(menu => menu.classList.add('.active-color'))
 
+    }
 
+    document.querySelectorAll("[data_dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove("active")
 
     })
+
+})
+levelTwo.addEventListener('click', () => {
+    subList.classList.toggle('show')
+
 })
